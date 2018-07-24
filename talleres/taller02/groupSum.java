c131e201ec64b738402ea8dc53e4289c9368f698
LauraGiraldo <41221790+LauraGiraldo@users.noreply.github.com>
2018-07-24 11:01:32 -0500

    Add files via upload

---
+++ talleres/taller02/groupSum.java
@@ -0,0 +1,8 @@
+
+public boolean groupSum(int start, int[] nums, int target) {
+
+
+return start>= nums.length ? target==0: groupSum(start+1,nums,target-nums[start]) || groupSum(start+1,nums,target); 
+
+
+}
