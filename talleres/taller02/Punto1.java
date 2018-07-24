41b2193304b117c7228b18acc1ce14992cc86d6c
afoquendou <41337721+afoquendou@users.noreply.github.com>
2018-07-24 10:57:40 -0500

    Add files via upload

---
+++ talleres/taller02/Punto1.java
@@ -0,0 +1,18 @@
+class Punto1
+{
+	
+ public static void main (String[] args){
+
+  System.out.println(MCD(345,150));
+
+ }
+
+ public static int MCD(int a, int b){
+
+  if (b == 0) return a;
+	
+  else return MCD(b,a%b);
+	
+ }
+
+}
