d6afcb0a5212e186b1dd20607b02b62023801e57
afoquendou <41337721+afoquendou@users.noreply.github.com>
2018-08-01 00:40:44 -0500

    Add files via upload

---
+++ talleres/taller03/permutacion.java
@@ -0,0 +1,28 @@
+public class Permutation {
+    
+ public static void main(String args[]) {
+
+      per("","ab");
+    
+ }
+
+
+ public static void per(String base, String s){
+ 
+     if (s.length() == 0) {
+ 
+       System.out.println(base);
+        
+     }
+
+ 
+     for (int i = 0; i < s.length(); i++)
+{
+
+       per(base + s.charAt(i), s.substring(0,i)+s.substring(i+1));
+  
+     }
+    
+ }
+
+}
