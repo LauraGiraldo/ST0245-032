43410e2e3eeecff4f1f5dd3906e7e044eeea6fd6
afoquendou <41337721+afoquendou@users.noreply.github.com>
2018-08-21 12:04:18 -0500

    Update insertionSort.java

--- talleres/taller05/insertionSort.java
+++ talleres/taller05/insertionSort.java
@@ -1,17 +1,18 @@
   public class Taller5
 {
-   public static void main(String[] args){
-      for (int i = 10000000; i <= 200000000; i = i + 10000000){
-        int[] a = new int[i];
-        for (int j = 0; j < i; j++ ){
-          a[j] = j;
+   public static void main(String[] args) {
+        
+        for (int i = 10000; i <= 200000; i = i + 10000) {
+            int[] a = new int[i];
+            for (int j = 0; j < i; j++) {
+                a[j] = (int) (Math.random()*j);
+            }
+            long start = System.currentTimeMillis();
+            insertionSort(a);
+            long fin = System.currentTimeMillis();
+            System.out.println(fin - start);
         }
-        long start = System.currentTimeMillis();
-          suma(a);
-        long fin = System.currentTimeMillis();       
-        System.out.println(fin-start);
-      }
-  }
+    }
   public static int[] insertionSort(int[] a) {
 
         for (int i = 0; i < a.length; i++) {
