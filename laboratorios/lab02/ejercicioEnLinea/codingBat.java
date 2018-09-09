e28eff6e5a8ce0e89f52776e3d943a20680fca58
LauraGiraldo <41221790+LauraGiraldo@users.noreply.github.com>
2018-09-08 21:38:11 -0500

    Update codingBat.java

--- laboratorios/lab02/ejercicioEnLinea/codingBat.java
+++ laboratorios/lab02/ejercicioEnLinea/codingBat.java
@@ -125,3 +125,24 @@ public int[] fix45(int[] nums) {
         }
         return aux;
     }
+
+
+public boolean canBalance(int[] nums) {
+ int sum1=0;
+
+ for(int i=0; i<nums.length; i++){
+   sum1+=nums[i];
+  
+   int sum2=0;
+ for(int j=nums.length-1; j > i;j--){
+   sum2+=nums[j];
+ }
+ if(sum1==sum2)
+ return true;
+ 
+ }
+
+ return false;
+}
+
+
