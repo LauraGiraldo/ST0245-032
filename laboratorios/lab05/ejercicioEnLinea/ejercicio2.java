2fa2adb6ca9755e80c44d946be6d32b80a981369
afoquendou <41337721+afoquendou@users.noreply.github.com>
2018-10-20 16:57:18 -0500

    Add files via upload

---
+++ laboratorios/lab05/ejercicioEnLinea/ejercicio2.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab5;
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+/**
+ *
+ * @author xakol
+ */
+public class ejercicio2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        while (n != 0) {
+            DigraphAL dgal = new DigraphAL(n);
+            int arc = sc.nextInt();
+            for (int i = 0; i < arc; i++) {
+                int n1 = sc.nextInt();
+                int n2 = sc.nextInt();
+                dgal.addArc(n1, n2, 1);
+            }
+            System.out.println(isBicolorable(dgal));
+            n = sc.nextInt();
+        }
+    }
+
+    public static String isBicolorable(Digraph g) {
+        int n = g.size;
+        int color[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            color[i] = -1;
+        }
+        color[0] = 1;
+        
+        
+            for (int j = 0; j < n; j++) {
+            for (int i = 0; i < n; i++) {
+                
+                if (g.getWeight(j, i) != 0 && color[i] == -1) {
+
+                    color[i] = 1 - color[j];
+                    
+                } else if (g.getWeight(j, i) != 0 && color[j] == color[i]) {
+                    return "NOT BICOLORABLE";
+                }
+            }
+        }
+            
+        
+        return "BICOLORABLE";
+    }
+}
